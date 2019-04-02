8bb0510da9a46fa8ccb1688fc71c82b741337881
AlexVotry <alexvotry@gmail.com>
2019-04-02 14:06:33 -0700

    added cssans

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,18 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <div className="menu">
-            <Link to="/" className="item">REACT</Link>
+            <Link to="/" className="item">
+                <div class="cssans cssans--center">
+                    <div class="cssans__accessible">CSSans Pro</div>
+                    <div class="cssans__word">
+                        <b class="cssans:R"></b>
+                        <b class="cssans:e"></b>
+                        <b class="cssans:a"></b>
+                        <b class="cssans:c"></b>
+                        <b class="cssans:t"></b>
+                    </div>
+                </div>
+            </Link>
             <div className="right menu">
                 <Link to="/" className="item">React</Link>
             </div>
